6fb3effcf4bb4d97f970bf7a8904a25579b6a532
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2024-03-12 21:56:20 -0500

    added max height and overflow

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,18 +10,20 @@ const About = () => {
         <div style={styles.Nav}>
             <Nav/>
         </div>
-        <div class='bg' style={{backgroundImage: `url(${spacebg})`}}>
-        <div style={styles.about}>
-            <div style={styles.intro}>
-                <h1 style={styles.name} class='name'>Brian Barker</h1>
-                <p style={styles.para}>Welcome! I am a hardworking veteran looking to enter the tech field.
-                I recently graduated from school for web development and design.
-                I have a passion for creating in the digital space and would love to find a company to grow my skills.</p>
+        <div className='body-space'>
+            <div class='bg' style={{backgroundImage: `url(${spacebg})`}}>
+            <div style={styles.about}>
+                <div style={styles.intro}>
+                    <h1 style={styles.name} class='name'>Brian Barker</h1>
+                    <p style={styles.para}>Welcome! I am a hardworking veteran looking to enter the tech field.
+                    I recently graduated from school for web development and design.
+                    I have a passion for creating in the digital space and would love to find a company to grow my skills.</p>
+                </div>
+                <div>
+                    <img src={me} alt='me' style={styles.img}/>
+                </div>
             </div>
-            <div>
-                <img src={me} alt='me' style={styles.img}/>
             </div>
-        </div>
         </div>
         </>
     )
@@ -32,7 +34,7 @@ export default About
 const styles={
     about:{
         display: 'flex',
-        flexDirection: 'row'
+        flexDirection: 'row',
     },
     img:{
         height: '1200px',
