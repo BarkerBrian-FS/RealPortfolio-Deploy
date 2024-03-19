0316bffad04e57cdac7a46b8d3377af4b8994c24
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2024-03-19 13:09:20 -0500

    changed the about section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -17,7 +17,7 @@ const About = () => {
                     <h1 style={styles.name} class='name'>Brian Barker</h1>
                     <p style={styles.para}>Welcome! I am a hardworking veteran looking to enter the tech field.
                     I recently graduated from school for web development and design.
-                    I have a passion for creating in the digital space and would love to find a company to grow my skills.</p>
+                    I have a passion for creating in the digital space and would love to find a company where I can contribute and grow my skills.</p>
                 </div>
                 <div>
                     <img src={me} alt='me' style={styles.img}/>
