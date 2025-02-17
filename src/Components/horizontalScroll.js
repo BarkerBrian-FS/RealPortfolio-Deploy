a784d36a0f9590e9fb1961716328617664dc606e
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-02-17 15:03:07 -0600

    working on horizontal scroll function

--- src/Components/horizontalScroll.js
+++ src/Components/horizontalScroll.js
@@ -2,20 +2,28 @@ import React,{ useRef } from 'react';
 import asteroid from '../videos/asteroids.mp4';
 import cryptoWatch from '../videos/cryptoWatch.mp4';
 import { motion, useScroll, useTransform } from 'framer-motion';
-import './videoDisplay.css';
+import './hScroll.css';
 
 const videoDisplay = () => {
-  return (<div className='carousel'>
+    const targetRef = useRef(null);
+    const { scrollYProgress } = useScroll({target:targetRef});
+    const x = useTransform(scrollYProgress, [0, 1], ['0%', '55%']);
+
+  return (<div className='carousel' ref={targetRef}>
     <div className='carousel__container'>
     <div className='carousel__item'>
 
-        <h1 className='carousel__title'>Asteroid Game</h1>
         
-        <video src={asteroid} controls width="250" alt='asteroid' className='carousel__video'/>
+        <div className='carousel__video'>
+            <h1 className='carousel__title'>Asteroid Game</h1>
+            <video src={asteroid} controls width="250" alt='asteroid' className='carousel__video'/>
+        </div>
 
-        <h1 className='carousel__title'>CryptoWatch</h1>
-
-        <video src={cryptoWatch} controls width="250" alt='cryptoWatch' className='carousel__video'/>
+        
+        <div className='carousel__video'>
+            <h1 className='carousel__title'>CryptoWatch</h1>
+            <video src={cryptoWatch} controls width="250" alt='cryptoWatch' className='carousel__video'/>
+        </div>
 
 
     </div>
