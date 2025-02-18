9c0d67b61157e58931eff2226591815a538e01b3
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-02-18 12:42:46 -0600

    added horizontal scroll will work on more later

--- src/Components/horizontalScroll.js
+++ src/Components/horizontalScroll.js
@@ -7,26 +7,32 @@ import './hScroll.css';
 const VideoDisplay = () => {
     const targetRef = useRef();
     const { scrollYProgress } = useScroll({target:targetRef});
-    const x = useTransform(scrollYProgress, [0, 1], ['0%', '55%']);
+    const x = useTransform(scrollYProgress, [0, 1], ['0%', '-50%']);
 
   return (<div className='carousel' ref={targetRef}>
     <div className='carousel__container'>
-    <div className='carousel__item'>
+    <motion.div className='carousel__item' style={{x}}>
 
         
-        <div className='carousel__video'>
+        <motion.div className='carousel__video'
+        initial={{opacity: 0, y: 150}}
+        whileInView={{opacity: 1, y: 0, threshold: 0.99}}
+        transition={{duration: 1, ease: 'easeOut'}}>
             <h1 className='carousel__title'>Asteroid Game</h1>
             <video src={asteroid} controls width="250" alt='asteroid' className='carousel__video'/>
-        </div>
+        </motion.div>
 
         
-        <div className='carousel__video'>
+        <motion.div className='carousel__video'
+        initial={{opacity: 0, y: 150}}
+        whileInView={{opacity: 1, y: 0, threshold: 0.99}}
+        transition={{duration: 1, ease: 'easeOut'}}>
             <h1 className='carousel__title'>CryptoWatch</h1>
             <video src={cryptoWatch} controls width="250" alt='cryptoWatch' className='carousel__video'/>
-        </div>
+        </motion.div>
 
 
-    </div>
+    </motion.div>
     </div>
         
 
--- src/Components/videoDisplay.js
+++ src/Components/videoDisplay.js
@@ -1,28 +1,39 @@
 import React,{ useRef } from 'react';
 import asteroid from '../videos/asteroids.mp4';
 import cryptoWatch from '../videos/cryptoWatch.mp4';
+import { motion } from 'framer-motion';
 
-/*const videoDisplay = () => {
+const VideoDisplay = () => {
+
   return (
     <div className='container'>
-        <div className='asteroid'>
+        <motion.div className='asteroid'
+            initial={{opacity: 0, y: 150}}
+            whileInView={{opacity: 1, y: 0, threshold: 0.99}}
+            transition={{duration: 1, ease: 'easeOut'}}>
             <h1 style={styles.title}>Asteroid Game</h1>
             <video src={asteroid} controls width="250" alt='asteroid' style={styles.asteroid}/>
-        </div>
-        <div className='cryptoWatch'>
+        </motion.div>
+        <motion.div className='cryptoWatch'
+            initial={{opacity: 0, y: 150}}
+            whileInView={{opacity: 1, y: 0, threshold: 0.99}}
+            transition={{duration: 1, ease: 'easeOut'}}>
             <h1 style={styles.title}>CryptoWatch</h1>
             <video src={cryptoWatch} controls width="250" alt='cryptoWatch' style={styles.cryptoWatch}/>
-        </div>
+        </motion.div>
     </div>
   );
 };
-export default videoDisplay
+export default VideoDisplay
 
 const styles = {
         container:{
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center'
+        alignItems: 'center',
+        overflow: 'hidden',
+        position: 'sticky',
+        top: 0
     },
     asteroid:{
         marginTop: '9%',
@@ -48,4 +59,4 @@ const styles = {
         marginBottom: '-8%',
         marginLeft: '38%'
     }
-};*/
+};
