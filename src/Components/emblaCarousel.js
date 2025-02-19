c99bb8fc5f690198b3d19c71ed07be3245cd110f
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-02-19 14:34:27 -0600

    finished styling video carousel

--- src/Components/emblaCarousel.js
+++ src/Components/emblaCarousel.js
@@ -15,7 +15,8 @@ const EmblaCarousel = (props) => {
     const options = { loop: false }
   const [emblaRef, emblaApi] = useEmblaCarousel(options, [Fade()])
 
-  const slides = [asteroids,cryptoWatch]
+  const slides = [{ src: asteroids, title: 'Asteroid Game' },
+    { src: cryptoWatch, title: 'CryptoWatch' }]
   const {
     prevBtnDisabled,
     nextBtnDisabled,
@@ -27,11 +28,12 @@ const EmblaCarousel = (props) => {
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
-          {slides.map((index) => (
+          {slides.map((slides, index) => (
             <div className="embla__slide" key={index}>
+            <div className = 'title'>{slides.title}</div>
               <video controls width="250"
                 className="embla__slide__img"
-                src={index}
+                src={slides.src}
               />
             </div>
           ))}
