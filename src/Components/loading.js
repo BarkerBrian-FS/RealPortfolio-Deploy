eb7f66e89ee9f512788881e24d356bd164e2aa33
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-02-24 14:29:24 -0600

    change lottie height

--- src/Components/loading.js
+++ src/Components/loading.js
@@ -25,7 +25,7 @@ const LoadingScreen = () => {
     return (
       isLoading && (
         <div className="loading" style={style.loadingScreen}>
-          <Lottie className="lottie" options={defaultOptions} height={400} width={400} />
+          <Lottie className="lottie" options={defaultOptions} height={600} width={600} />
         </div>
       )
     );
