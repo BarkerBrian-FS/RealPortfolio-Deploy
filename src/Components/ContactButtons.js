2f91b59ecc45e1c3dd6b819bda286ef6ca50522c
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2025-02-26 14:35:56 -0600

    added media queries for 425px for contact form

--- src/Components/ContactButtons.js
+++ src/Components/ContactButtons.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import github from '../images/github.png';
 import linkedIn from '../images/linkedin.png';
-
+import './Contact.css';
 const ContactButtons = () => {
   return (
     <div>
-        <div style={styles.contactBtn}>
+        <div style={styles.contact}>
             <motion.div
-                whileHover={{ scale: 1.1 }} style={styles.github}>
+                whileHover={{ scale: 1.1 }} style={styles.github} className='github'>
                     <a href='https://github.com/BarkerBrian-FS?tab=repositories'><img  style={styles.github} src={github} alt='github'/></a>
             </motion.div>
             <motion.div
-                whileHover={{ scale: 1.1 }}>
+                whileHover={{ scale: 1.1 }} className='linkedIn'>
                     <a href='https://www.linkedin.com/in/brian-barker-498537202/'><img  style={styles.linkedIn} src={linkedIn}  alt='linkedIn'/></a>
             </motion.div>
         </div>
@@ -35,13 +35,12 @@ const styles = {
       width: '105px',
       marginLeft: '10px'
   },
-  contactBtn:{
+  contact:{
       display: 'flex',
       flexDirection: 'row',
       marginLeft: '43%',
       marginRight: 'auto',
       marginTop: '40px',
       justifyContent: 'spaceBetween',
-  },
-
+  }
 }
